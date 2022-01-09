555caec0752c4b18def1f71a2ad8770df3dc3438
samarjeet saurabh <saurabhsamarjeet@gmail.com>
2022-01-09 20:10:03 +0530

    Update first_ec2.tf

--- EC2/first_ec2.tf
+++ EC2/first_ec2.tf
@@ -10,8 +10,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAQZ4EJHNIBNFZ3DVR"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 resource "aws_instance" "myec2" {
     ami="ami-047a51fa27710816e"
